3574a0354f8e010f16a4b5f94b09328800675128
poproar <poproar@users.noreply.github.com>
2020-03-15 19:47:34 -0700

    need more?

---
+++ js/code.js
@@ -0,0 +1,354 @@
+/**
+ * @license
+ * Copyright 2012 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview JavaScript for Blockly's Code demo.
+ * @author fraser@google.com (Neil Fraser)
+ */
+'use strict';
+
+Blockly.Msg["MATH_HUE"] = "230";
+Blockly.Msg["LOOPS_HUE"] = "120";
+Blockly.Msg["LISTS_HUE"] = "260";
+Blockly.Msg["LOGIC_HUE"] = "210";
+Blockly.Msg["VARIABLES_HUE"] = "330";
+Blockly.Msg["TEXTS_HUE"] = "160";
+Blockly.Msg["PROCEDURES_HUE"] = "290";
+Blockly.Msg["COLOUR_HUE"] = "20";
+Blockly.Msg["VARIABLES_DYNAMIC_HUE"] = "310";
+
+/**
+ * Create a namespace for the application.
+ */
+var Code = {};
+
+/**
+ * Lookup for names of supported languages.  Keys should be in ISO 639 format.
+ */
+Code.LANGUAGE_NAME = {
+  'ar': 'العربية',
+  'be-tarask': 'Taraškievica',
+  'br': 'Brezhoneg',
+  'ca': 'Català',
+  'cs': 'Česky',
+  'da': 'Dansk',
+  'de': 'Deutsch',
+  'el': 'Ελληνικά',
+  'en': 'English',
+  'es': 'Español',
+  'et': 'Eesti',
+  'fa': 'فارسی',
+  'fr': 'Français',
+  'he': 'עברית',
+  'hrx': 'Hunsrik',
+  'hu': 'Magyar',
+  'ia': 'Interlingua',
+  'is': 'Íslenska',
+  'it': 'Italiano',
+  'ja': '日本語',
+  'kab': 'Kabyle',
+  'ko': '한국어',
+  'mk': 'Македонски',
+  'ms': 'Bahasa Melayu',
+  'nb': 'Norsk Bokmål',
+  'nl': 'Nederlands, Vlaams',
+  'oc': 'Lenga d\'òc',
+  'pl': 'Polski',
+  'pms': 'Piemontèis',
+  'pt-br': 'Português Brasileiro',
+  'ro': 'Română',
+  'ru': 'Русский',
+  'sc': 'Sardu',
+  'sk': 'Slovenčina',
+  'sr': 'Српски',
+  'sv': 'Svenska',
+  'ta': 'தமிழ்',
+  'th': 'ภาษาไทย',
+  'tlh': 'tlhIngan Hol',
+  'tr': 'Türkçe',
+  'uk': 'Українська',
+  'vi': 'Tiếng Việt',
+  'zh-hans': '简体中文',
+  'zh-hant': '正體中文'
+};
+
+/**
+ * List of RTL languages.
+ */
+Code.LANGUAGE_RTL = ['ar', 'fa', 'he', 'lki'];
+
+/**
+ * Blockly's main workspace.
+ * @type {Blockly.WorkspaceSvg}
+ */
+Code.workspace = null;
+
+/**
+ * Extracts a parameter from the URL.
+ * If the parameter is absent default_value is returned.
+ * @param {string} name The name of the parameter.
+ * @param {string} defaultValue Value to return if parameter not found.
+ * @return {string} The parameter value or the default value if not found.
+ */
+Code.getStringParamFromUrl = function(name, defaultValue) {
+  var val = location.search.match(new RegExp('[?&]' + name + '=([^&]+)'));
+  return val ? decodeURIComponent(val[1].replace(/\+/g, '%20')) : defaultValue;
+};
+
+/**
+ * Get the language of this user from the URL.
+ * @return {string} User's language.
+ */
+Code.getLang = function() {
+  var lang = Code.getStringParamFromUrl('lang', '');
+  if (Code.LANGUAGE_NAME[lang] === undefined) {
+    // Default to English.
+    lang = 'en';
+  }
+  return lang;
+};
+
+/**
+ * Is the current language (Code.LANG) an RTL language?
+ * @return {boolean} True if RTL, false if LTR.
+ */
+Code.isRtl = function() {
+  return Code.LANGUAGE_RTL.indexOf(Code.LANG) != -1;
+};
+
+/**
+ * Load blocks saved on App Engine Storage or in session/local storage.
+ * @param {string} defaultXml Text representation of default blocks.
+ */
+Code.loadBlocks = function(defaultXml) {
+  try {
+    var loadOnce = window.sessionStorage.loadOnceBlocks;
+  } catch(e) {
+    // Firefox sometimes throws a SecurityError when accessing sessionStorage.
+    // Restarting Firefox fixes this, so it looks like a bug.
+    var loadOnce = null;
+  }
+  if ('BlocklyStorage' in window && window.location.hash.length > 1) {
+    // An href with #key trigers an AJAX call to retrieve saved blocks.
+    BlocklyStorage.retrieveXml(window.location.hash.substring(1));
+  } else if (loadOnce) {
+    // Language switching stores the blocks during the reload.
+    delete window.sessionStorage.loadOnceBlocks;
+    var xml = Blockly.Xml.textToDom(loadOnce);
+    Blockly.Xml.domToWorkspace(xml, Code.workspace);
+  } else if (defaultXml) {
+    // Load the editor with default starting blocks.
+    var xml = Blockly.Xml.textToDom(defaultXml);
+    Blockly.Xml.domToWorkspace(xml, Code.workspace);
+  } else if ('BlocklyStorage' in window) {
+    // Restore saved blocks in a separate thread so that subsequent
+    // initialization is not affected from a failed load.
+    window.setTimeout(BlocklyStorage.restoreBlocks, 0);
+  }
+};
+
+/**
+ * Save the blocks and reload with a different language.
+ */
+Code.changeLanguage = function() {
+  // Store the blocks for the duration of the reload.
+  // MSIE 11 does not support sessionStorage on file:// URLs.
+  if (window.sessionStorage) {
+    var xml = Blockly.Xml.workspaceToDom(Code.workspace);
+    var text = Blockly.Xml.domToText(xml);
+    window.sessionStorage.loadOnceBlocks = text;
+  }
+
+  var languageMenu = document.getElementById('languageMenu');
+  var newLang = encodeURIComponent(
+      languageMenu.options[languageMenu.selectedIndex].value);
+  var search = window.location.search;
+  if (search.length <= 1) {
+    search = '?lang=' + newLang;
+  } else if (search.match(/[?&]lang=[^&]*/)) {
+    search = search.replace(/([?&]lang=)[^&]*/, '$1' + newLang);
+  } else {
+    search = search.replace(/\?/, '?lang=' + newLang + '&');
+  }
+
+  window.location = window.location.protocol + '//' +
+      window.location.host + window.location.pathname + search;
+};
+/**
+ * Bind a function to a button's click event.
+ * On touch enabled browsers, ontouchend is treated as equivalent to onclick.
+ * @param {!Element|string} el Button element or ID thereof.
+ * @param {!Function} func Event handler to bind.
+ */
+Code.bindClick = function(el, func) {
+  if (typeof el == 'string') {
+    el = document.getElementById(el);
+  }
+  el.addEventListener('click', func, true);
+  el.addEventListener('touchend', func, true);
+};
+/**
+ * User's language (e.g. "en").
+ * @type {string}
+ */
+Code.LANG = Code.getLang();
+/**
+ * Initialize Blockly.  Called on page load.
+ */
+Code.init = function() {
+  Code.initLanguage();
+
+  var rtl = Code.isRtl();
+  for (var messageKey in MSG) {
+    if (messageKey.indexOf('cat') == 0) {
+      Blockly.Msg[messageKey.toUpperCase()] = MSG[messageKey];
+    }
+  }
+  // Construct the toolbox XML, replacing translated variable names.
+  var toolboxText = document.getElementById('toolbox').outerHTML;
+  toolboxText = toolboxText.replace(/(^|[^%]){(\w+)}/g,
+      function(m, p1, p2) {return p1 + MSG[p2];});
+  var toolboxXml = Blockly.Xml.textToDom(toolboxText);
+
+  Code.workspace = Blockly.inject('blocklyDiv',
+      {grid:
+          {spacing: 25,
+           length: 3,
+           colour: '#ccc',
+           snap: true},
+       media: '../../media/',
+       rtl: rtl,
+       toolbox: toolboxXml,
+       zoom:
+           {controls: true,
+            wheel: true}
+      });
+
+  // Add to reserved word list: Local variables in execution environment (runJS)
+  // and the infinite loop detection function.
+  Blockly.JavaScript.addReservedWords('code,timeouts,checkTimeout');
+
+  Code.loadBlocks('');
+
+  if ('BlocklyStorage' in window) {
+    // Hook a save function onto unload.
+    BlocklyStorage.backupOnUnload(Code.workspace);
+  }
+
+  Code.bindClick('trashButton',
+      function() {Code.discard(); Code.renderContent();});
+  Code.bindClick('runButton', Code.runJS);
+  // Disable the link button if page isn't backed by App Engine storage.
+  var linkButton = document.getElementById('linkButton');
+  if ('BlocklyStorage' in window) {
+    BlocklyStorage['HTTPREQUEST_ERROR'] = MSG['httpRequestError'];
+    BlocklyStorage['LINK_ALERT'] = MSG['linkAlert'];
+    BlocklyStorage['HASH_ERROR'] = MSG['hashError'];
+    BlocklyStorage['XML_ERROR'] = MSG['xmlError'];
+    Code.bindClick(linkButton,
+        function() {BlocklyStorage.link(Code.workspace);});
+  } else if (linkButton) {
+    linkButton.className = 'disabled';
+  }
+
+};
+/**
+ * Initialize the page language.
+ */
+Code.initLanguage = function() {
+  // Set the HTML's language and direction.
+  var rtl = Code.isRtl();
+  document.dir = rtl ? 'rtl' : 'ltr';
+  document.head.parentElement.setAttribute('lang', Code.LANG);
+
+  // Sort languages alphabetically.
+  var languages = [];
+  for (var lang in Code.LANGUAGE_NAME) {
+    languages.push([Code.LANGUAGE_NAME[lang], lang]);
+  }
+  var comp = function(a, b) {
+    // Sort based on first argument ('English', 'Русский', '简体字', etc).
+    if (a[0] > b[0]) return 1;
+    if (a[0] < b[0]) return -1;
+    return 0;
+  };
+  languages.sort(comp);
+  // Populate the language selection menu.
+  var languageMenu = document.getElementById('languageMenu');
+  languageMenu.options.length = 0;
+  for (var i = 0; i < languages.length; i++) {
+    var tuple = languages[i];
+    var lang = tuple[tuple.length - 1];
+    var option = new Option(tuple[0], lang);
+    if (lang == Code.LANG) {
+      option.selected = true;
+    }
+    languageMenu.options.add(option);
+  }
+  languageMenu.addEventListener('change', Code.changeLanguage, true);
+
+  // Inject language strings.
+  document.title += ' ' + MSG['title'];
+  document.getElementById('title').textContent = MSG['title'];
+  document.getElementById('blocksTab').textContent = MSG['blocks'];
+
+  document.getElementById('linkButton').title = MSG['linkTooltip'];
+  document.getElementById('runButton').title = MSG['runTooltip'];
+  document.getElementById('trashButton').title = MSG['trashTooltip'];
+};
+
+/**
+ * Execute the user's code.
+ * Just a quick and dirty eval.  Catch infinite loops.
+ */
+Code.runJS = function() {
+  Blockly.JavaScript.INFINITE_LOOP_TRAP = 'checkTimeout();\n';
+  var timeouts = 0;
+  var checkTimeout = function() {
+    if (timeouts++ > 1000000) {
+      throw MSG['timeout'];
+    }
+  };
+  var code = Blockly.JavaScript.workspaceToCode(Code.workspace);
+  Blockly.JavaScript.INFINITE_LOOP_TRAP = null;
+  try {
+    eval(code);
+  } catch (e) {
+    alert(MSG['badCode'].replace('%1', e));
+  }
+};
+
+/**
+ * Discard all blocks from the workspace.
+ */
+Code.discard = function() {
+  var count = Code.workspace.getAllBlocks(false).length;
+  if (count < 2 ||
+      window.confirm(Blockly.Msg['DELETE_ALL_BLOCKS'].replace('%1', count))) {
+    Code.workspace.clear();
+    if (window.location.hash) {
+      window.location.hash = '';
+    }
+  }
+};
+
+// Load the Code demo's language strings.
+document.write('<script src="msg/' + Code.LANG + '.js"></script>\n');
+// Load Blockly's language strings.
+document.write('<script src="../../msg/js/' + Code.LANG + '.js"></script>\n');
+
+window.addEventListener('load', Code.init);
